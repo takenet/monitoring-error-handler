5ddb38031d7e481790cfed48149528dd37ee03bb
samwx <samuelmartins.sw@gmail.com>
2018-09-27 10:50:46 -0300

    chore: Removed dist folder from future commits

--- dist/index.d.ts
+++
@@ -1,1 +0,0 @@
-export { MonitoringErrorHandler } from './MonitoringErrorHandler';
--- dist/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var MonitoringErrorHandler_1 = require("./MonitoringErrorHandler");
-exports.MonitoringErrorHandler = MonitoringErrorHandler_1.MonitoringErrorHandler;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi9zcmMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7QUFBQSxtRUFBa0U7QUFBekQsMERBQUEsc0JBQXNCLENBQUEifQ==
